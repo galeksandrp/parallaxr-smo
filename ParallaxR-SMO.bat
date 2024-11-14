5f2d37a2be0512a8e10ba0572abc5771b8ffc505
Alexander Georgievskiy <galeksandrp@gmail.com>
2024-11-14 18:00:31 +0300

    v1.4

--- ParallaxR-SMO.bat
+++ ParallaxR-SMO.bat
@@ -4,13 +4,14 @@
 ::    |    |     / __ \|  | \// __ \|  |_|  |__/ __ \_>    < |    |   \
 ::    |____|    (____  /__|  (____  /____/____(____  /__/\_ \|____|_  /
 ::                   \/           \/               \/      \/       \/ 
-::                                                 by gavwhittaker
+::                              Single Mod Optimiser by gavwhittaker
 
 :: THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
 
 :: Changelog - Nexus Published
 :: ---------------------------
 
+:: v1.4		- Added BSA extract, Removed Select TEXTURES Folder, Improved Logging
 :: v1.3		- Added choose parallax strength, bugfix changed Output approach
 :: v1.2		- Improved logging, implemented user Parallax Intensity selection
 :: v1.1		- Early logging, guidance ref pathname special characters and progression
@@ -25,13 +26,13 @@
 :: BRC under non-commercial use license agreement
 :: torum for Image-viewer
 :: TheGuardianDovahkiin and BennyDollaz for testing and feedback
+:: AlexxEG for BSA Browser
 
 :: Variables and Temp Files
 ::-------------------------
 
 ::StartDir	= The folder where the ParallaxR BAT is run from
 ::pIntensity	= The user selected parallax intensity choice
-::TempDir	= The temporary directory where files are copied to and processed
 ::fchoose	= The user selected folder for processing (used twice)
 ::ModName	= The main folder (name) of the mod to process
 ::moddir.tmp	= Temporary file for modname processing for output folder name
@@ -45,13 +46,13 @@
 @echo off
 set StartDir="%CD%"
 cd tools
+set ToolsDir="%CD%"
 taskkill /f /im powershell.exe >NUL 2>&1
 setlocal enabledelayedexpansion
 imageviewer -f on title.jpg
 color 8F
 title ParallaxR
-cd Output
-set TempDir="%CD%"
+cd /d %StartDir%
 start /min /low powershell.exe -ExecutionPolicy Bypass -File .\AntiSleep.ps1
 cls
 echo.
@@ -61,7 +62,7 @@ echo ParallaxR session started at %time% >%StartDir%\logfile.txt
 echo.
 echo Choose below your desired parallax strength (how pronounced the 3D effect is)
 echo.
-echo The LOW setting is the mod authors preference and gives an understated 3D effect
+echo The LOW setting is the mod authors preference an understated realistic 3D effect
 echo MEDIUM delivers a great effect overall although expect occasional artifacts
 echo HIGH ensures a strong parallax effect although can sometimes look unrealistic
 echo.
@@ -72,8 +73,7 @@ if %ErrorLevel%==1 set pIntensity=-88
 echo P-Int is %pIntensity% >>%StartDir%\logfile.txt
 
 echo.
-echo A pop up window will appear, find the mod to process inside your Mod Manager\Mods folder
-echo Be sure to highlight the TEXTURES subfolder and then click OK to continue
+echo A pop up window will appear, highlight the mod to process inside your Mod Manager\Mods folder
 echo.
 echo WARNING: if the full path to your selected mod contains special characters such as 
 echo brackets and parenthesis then ParallaxR may exit without warning and fail. To remedy
@@ -84,13 +84,15 @@ pause
 
 title Select Folder
 set "psCommand="(new-object -COM 'Shell.Application')^
-.BrowseForFolder(0,'IMPORTANT: Find the mod inside your Mod Manager\Mods folder. Be sure to highlight the TEXTURES subfolder and then click OK to continue',0,0).self.path""
+.BrowseForFolder(0,'IMPORTANT: Highlight the mod inside your Mod Manager\Mods folder and then click OK to continue',0,0).self.path""
 call :OpenFolderBox
 if "%fchoose%" == "" goto :Terminate
 echo Chosen input folder is %fchoose% >>%StartDir%\logfile.txt
+echo Chosen input folder is %fchoose%
 
-cd /d !fchoose! >NUL 2>&1
-cd ..
+set Mod="%fchoose%"
+
+cd /d %Mod% >NUL 2>&1
 set ModName="%CD%"
 
 for /f "delims=" %%i IN (%ModName%) do (
@@ -101,14 +103,34 @@ for /f "delims=" %%i IN (%ModName%) do (
 title ParallaxR
 set ModName=%ModName: =%
 
-cd /d %TempDir%
+cd /d %StartDir%
 mkdir %ModName%
 cd /d %ModName%
-mkdir textures
-cd textures
-set ModDir="%CD%"
 
-cd /d !fchoose! >NUL 2>&1
+::---------------
+
+title Processing BSAs
+echo Processing BSA archives
+echo Processing BSA archives >>%StartDir%\logfile.txt
+
+copy %Mod%\*.bsa .\ >NUL 2>&1
+forfiles /m *.bsa /c "%ToolsDir%\bsab.exe -i --mtc -e -l:f -f *_n.dds @file .\" >NUL 2>&1
+forfiles /m *.bsa /c "%ToolsDir%\bsab.exe -i --mtc -e -l:f -f *_n.dds @file .\" >>%StartDir%\logfile.txt
+
+forfiles /m *.bsa /c "%ToolsDir%\bsab.exe -i --mtc -e -l:f -f *_p.dds @file .\" >NUL 2>&1
+forfiles /m *.bsa /c "%ToolsDir%\bsab.exe -i --mtc -e -l:f -f *_p.dds @file .\" >>%StartDir%\logfile.txt
+
+del *.bsa >NUL 2>&1
+
+::---------------
+
+echo Reviewing Loose files
+robocopy %Mod% .\ *.dds /MT:8 /S /LOG+:%StartDir%\logfile.txt >NUL 2>&1
+cd /d %StartDir%
+rename %ModName% ParallaxR-%ModName%
+cd /d "ParallaxR-%ModName%"
+
+::---------------
 
 set /a nCount=0
 set /a pCount=0
@@ -123,26 +145,19 @@ if %pCount% LSS %nCount% goto GoParallaxR
 color 4F
 title Error
 echo ERROR - Your selected mod does not appear to need any parallax files creating
-echo ERROR - Your selected mod does not appear to need any parallax files creating >>%StartDir%\logfile.txt
+echo EXITING - Your selected mod does not appear to need any parallax files creating >>%StartDir%\logfile.txt
 echo.
 echo ParallaxR will now exit
 echo.
 taskkill /f /im powershell.exe >NUL 2>&1
-cd /d %TempDir%
-cd ..
-rd /s /q .\temp
-mkdir temp
+cd /d %StartDir%
+rd /s /q "ParallaxR-%ModName%"
 pause
 exit
 
 ::---------------
 
 :GoParallaxR
-echo Copying files
-robocopy .\ %ModDir% *.dds /MT:8 /S /LOG+:%StartDir%\logfile.txt >NUL 2>&1
-cd /d %TempDir%
-rename %ModName% ParallaxR-%ModName%
-
 echo Cleansing files
 attrib +r *_n.dds /s >NUL 2>&1
 attrib +r *_p.dds /s >NUL 2>&1
@@ -154,18 +169,20 @@ for /f "delims=" %%d in ('dir /s /b /ad ^| sort /r') do rd "%%d" >NUL 2>&1
 ::---------------
 
 echo Preparing %nCount% Normal Maps
-for /r %%g in (*_n.dds) do ..\convert "%%g" -alpha off -background rgba(0,0,0,0) -resize 2048x2048 "%%~dpng.png" >NUL 2>&1 | title Resizing Normal :- %%~ng
+echo Preparing %nCount% Normal Maps >>%StartDir%\logfile.txt
+for /r %%g in (*_n.dds) do %ToolsDir%\convert "%%g" -alpha off -background rgba(0,0,0,0) -resize 2048x2048 "%%~dpng.png" >NUL 2>&1 | title Resizing Normal :- %%~ng
 del *_n.dds /s /q >NUL 2>&1
 
 echo Preserving %pCount% Existing Parallax Maps
+echo Preserving %pCount% Existing Parallax Maps >>%StartDir%\logfile.txt
 title Please Wait
-..\texconv -r:keep -pow2 -ft TIF -sepalpha -nologo -h 2048 *_p.dds >NUL 2>&1
+%ToolsDir%\texconv -r:keep -pow2 -ft TIF -sepalpha -nologo -h 2048 *_p.dds >NUL 2>&1
 del *_p.dds /s /q >NUL 2>&1
 
-..\brc64 /pattern:*_n.png /removelastn:2 /recursive /quiet /execute
-..\brc64 /pattern:*_N.png /removelastn:2 /recursive /quiet /execute >NUL 2>&1
-..\brc64 /pattern:*_p.tif /removelastn:2 /recursive /quiet /execute >NUL 2>&1
-..\brc64 /pattern:*_P.tif /removelastn:2 /recursive /quiet /execute >NUL 2>&1
+%ToolsDir%\brc64 /pattern:*_n.png /removelastn:2 /recursive /quiet /execute
+%ToolsDir%\brc64 /pattern:*_N.png /removelastn:2 /recursive /quiet /execute >NUL 2>&1
+%ToolsDir%\brc64 /pattern:*_p.tif /removelastn:2 /recursive /quiet /execute >NUL 2>&1
+%ToolsDir%\brc64 /pattern:*_P.tif /removelastn:2 /recursive /quiet /execute >NUL 2>&1
 
 ::---------------
 
@@ -173,23 +190,26 @@ for /r %%g in (*.png) do if exist "%%~dpng.tif" del "%%g" >NUL 2>&1 | del "%%~dp
 
 set /a cCount=nCount-pCount
 echo Creating %cCount% Parallax Height Maps
-for /r %%g in (*.png) do ..\NormalToHeight "%%g" "%%~dpng_p.png" -scale 100.00 -numPasses 32 -normalScale 1.00 -maxStepHeight 1 -mapping XrYfgZb -zrange full -edgeMode Wrap >NUL 2>&1 | title Writing Height File :- %%~ng
+echo Creating %cCount% Parallax Height Maps >>%StartDir%\logfile.txt
+for /r %%g in (*.png) do %ToolsDir%\NormalToHeight "%%g" "%%~dpng_p.png" -scale 100.00 -numPasses 32 -normalScale 1.00 -maxStepHeight 1 -mapping XrYfgZb -zrange full -edgeMode Wrap >NUL 2>&1 | title Writing Height File :- %%~ng
 
-for /r %%g in (*_p.png) do ..\convert -brightness-contrast -25X%pIntensity% "%%g" "%%~dpng.png" | title Optimising :- %%~ng
+for /r %%g in (*_p.png) do %ToolsDir%\convert -brightness-contrast -25X%pIntensity% "%%g" "%%~dpng.png" | title Optimising :- %%~ng
 
 echo Preparing 1k DDS Parallax Height Maps
-..\texconv -r:keep -pow2 -f BC4_UNORM -sepalpha -nologo -y -h 1024 *_p.png >NUL 2>&1
+echo Preparing 1k DDS Parallax Height Maps >>%StartDir%\logfile.txt
+%ToolsDir%\texconv -r:keep -pow2 -f BC4_UNORM -sepalpha -nologo -y -h 1024 *_p.png >NUL 2>&1
 del *.png /s /q >NUL 2>&1
 
 ::------------------------------------
 
-cd /d %TempDir%
-explorer %TempDir%
+cd /d %StartDir%
+explorer %StartDir%
 
 ::------------------------------------
 
 :Completed
 title Complete
+echo Completed >>%StartDir%\logfile.txt
 color 20
 echo.
 echo Completed at %time%
